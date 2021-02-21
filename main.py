a3fccf87ab12bd3a15ee1130bf40b932b4689cd5
Franziska Halischafsky <franziska.halischafsky@closed.com>
2021-02-21 20:10:09 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from sqlConnector import sql_Connector
+#! from sql_connect_unittest import Test_Connector
+#! from parsePara import parameter_help
+import logging
+
+sqlCon = sql_Connector()
+sqlCon.generate("output.csv")
+
+
+logging.basicConfig(
+    filename="test.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
+
+
+#! parsePa = parameter_help
+#! parsePa.parseHelp()
+
+#! testCase = Test_Connector
+#! testCase.test_connector()
